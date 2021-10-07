e481db5fa22e96b85e5f8b3b3dc4cf5b6a873519
SamuelSEV <SamuelSEV@github.com>
2021-10-07 12:17:29 +0200

    ejercicio 16

---
+++ ejercicio16.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 16</title>
+</head>
+<body>
+    <?php
+        include 'funciones.inc.php';
+        include_once 'funciones2.inc.php';
+        require 'funciones2.inc.php';
+        echo multiplicacion(3,7);
+    ?>
+</body>
+</html>
